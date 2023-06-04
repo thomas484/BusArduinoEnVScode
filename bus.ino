f85848156df25a1daac24560295ff5acaa6ac45d
Thomas Alvarez <thomasrmf96alvarez@gmial.com>
2023-06-04 01:04:19 -0300

    v1

--- bus.ino
+++ bus.ino
@@ -1,1 +1,11 @@
 //bus para probar motores 
+void setup(){
+pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop(){
+digitalWrite(LED_BUILTIN, HIGH);
+delay(1000);
+digitalWrite(LED_BUILTIN, LOW);
+delay(1000);
+
+}
